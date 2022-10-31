918454b15f6bb387f58db0d3536bdfd42f4297e6
shaikshan <shaikroshan1997@gmail.com>
2022-10-31 12:41:29 +0530

    Updated

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+import logging
 from sensor.configuration.mongo_db_connection import MongoDBClient
-
+from sensor.exception import SensorException
+from sensor.logger import logging
+import os,sys
+def main():
+    try:
+        x = 1/0
+        logging.info("dividing by zero")
+    except Exception as e:
+        raise SensorException(e,sys)
 if __name__ == "__main__":
-    moongodb_client = MongoDBClient()
-    print("collection_names:",moongodb_client.database.list_collection_names())
+    try:
+        main()
+    except Exception as e:
+        print(e)
+    #moongodb_client = MongoDBClient()
+    #print("collection_names:",moongodb_client.database.list_collection_names())
--- sensor/configuration/mongo_db_connection.py
+++ sensor/configuration/mongo_db_connection.py
@@ -4,13 +4,13 @@ from sensor.constants.database import DATABASE_NAME
 import certifi
 ca = certifi.where()
 import os
-
+from sensor.constants.env_variable import MONGODB_URL_KEY
 class MongoDBClient:
     client = None
     def __init__(self,database_name=DATABASE_NAME):
         try:
             if MongoDBClient.client is None:
-                mongo_db_url = os.getenv('MONGO_DB_URL',None)
+                mongo_db_url = os.getenv(MONGODB_URL_KEY)
                 MongoDBClient.client = pymongo.MongoClient(mongo_db_url,tlsCAFile=ca)
                 self.client = MongoDBClient.client
                 self.database = self.client[database_name]
