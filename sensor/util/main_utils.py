5db3aa0d8edaa568431cfeafc9062aa0c5bd0919
shaikshan <shaikroshan1997@gmail.com>
2022-11-02 20:45:00 +0530

    Updated data_transformation_config & main_util.py file

--- sensor/util/main_utils.py
+++ sensor/util/main_utils.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 import yaml
 import os,sys
+import dill
 
 def read_yaml_file(file_path:str)->dict:
     try:
@@ -21,5 +22,39 @@ def write_yaml_file(file_path:str,content:object,replace:bool=False):
         with open(file_path,'w') as yaml_file:
             yaml.dump(content,yaml_file)
 
+    except Exception as e:
+        raise SensorException(e,sys)
+
+def save_numpy_array_data(file_path:str,array:np.array):
+    """
+
+    """
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path,exist_ok=True)
+        with open(file_path,"wb") as file_obj:
+            np.save(file_obj,array)
+    except Exception as e:
+        raise SensorException(e,sys)
+    
+def load_numpy_array_data(file_path:str,)->np.array:
+    """
+    load numpy array data from file
+    file_path: str location of file to load
+    return: np.array data loaded
+    """
+    try:
+        with open(file_path,"rb") as file_obj:
+            return np.load(file_obj)
+    except Exception as e:
+        raise SensorException(e,sys)
+
+def save_object(file_path:str,obj:object)->None:
+    try:
+        logging.info("Entered the save object method of MainUtils class")
+        os.makedirs(os.path.dirname(file_path),exist_ok=True)
+        with open(file_path,'wb') as file_obj:
+            dill.dump(obj,file_obj)
+        logging.info("Exited the save object method of MainUtils class")
     except Exception as e:
         raise SensorException(e,sys)
